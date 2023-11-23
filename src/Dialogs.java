77342abfb4b9dc6e399fd9a575b9044c3a67b1bd
Vacor0911 <vactor0911@gmail.com>
2023-11-24 00:00:07 +0900

    Initial commit

--- src/Dialogs.java
+++ src/Dialogs.java
@@ -1,9 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
+
 import java.awt.event.*;
 
 public class Dialogs extends JDialog {
 	private static final long serialVersionUID = 1L;
+	
+	private JPanel pnlNorth = new JPanel();
+	private JLabel lblTitle = new JLabel("", SwingConstants.CENTER);
 	
 	public Dialogs(JFrame frame, String name, Dimension dim) {
 		super(frame, "", true);
@@ -18,22 +23,25 @@ public class Dialogs extends JDialog {
 		int x = res.width / 2 - dim.width / 2;
 		int y = res.height / 2 - dim.height / 2;
 		setLocation(x, y);
-	}
-}
-
-//문제집 - [검색]
-class WbSearchDlg extends Dialogs implements ActionListener {
-	private static final long serialVersionUID = 1L;
-	
-	public WbSearchDlg(JFrame frame) {
-		super( frame, "문제집 검색", new Dimension(400, 600) );
 		
 		Container c = getContentPane();
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
 		
+		c.add(pnlNorth, BorderLayout.NORTH);
+		pnlNorth.setLayout( new FlowLayout(FlowLayout.CENTER, 0, 5) );
+		pnlNorth.setBackground( Frame.getColor() );
+		pnlNorth.setBorder( new MatteBorder( 0,0,2,0,Frame.getColorBorder() ) );
+		pnlNorth.setPreferredSize( new Dimension(0, 50) );
+		pnlNorth.add(lblTitle);
+		
+		lblTitle.setFont( new Font(Frame.getFontName(), Font.BOLD, 26) );
+	}
+	
+	public JPanel getPanel() {
+		return pnlNorth;
+	}
+	
+	public JLabel getLabel() {
+		return lblTitle;
 	}
 }
 
@@ -41,10 +49,28 @@ class WbSearchDlg extends Dialogs implements ActionListener {
 class WbSortDlg extends Dialogs implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	
+	private JLabel lblTitle = getLabel();
+	private JPanel pnlCenter = new JPanel();
+	
 	public WbSortDlg(JFrame frame) {
 		super( frame, "문제집 정렬", new Dimension(400, 600) );
+		lblTitle.setText("정렬");
 		
 		Container c = getContentPane();
+		c.add(pnlCenter, BorderLayout.CENTER);
+		
+		pnlCenter.setLayout( new GridLayout(5, 1, 0 ,0) );
+		for(int i=0; i<5; i++) {
+			JButton btnTemp = new JButton( Integer.toString(i) );
+			btnTemp.setLayout( new BorderLayout() );
+			JPanel pnlTemp = new JPanel();
+			JPanel pnlTemp2 = new JPanel();
+			btnTemp.add(pnlTemp, BorderLayout.NORTH);
+			btnTemp.add(pnlTemp2, BorderLayout.SOUTH);
+			pnlTemp.setBackground(Color.red);
+			pnlTemp2.setBackground(Color.green);
+			pnlCenter.add(btnTemp);
+		}
 	}
 
 	@Override
@@ -56,9 +82,13 @@ class WbSortDlg extends Dialogs implements ActionListener {
 //문제집 - [문제집 추가]
 class WbAddDlg extends Dialogs implements ActionListener {
 	private static final long serialVersionUID = 1L;
+	
+	private JLabel lblTitle = getLabel();
+	private JPanel pnlCenter = new JPanel();
 		
 	public WbAddDlg(JFrame frame) {
 		super( frame, "문제집 추가", new Dimension(400, 600) );
+		lblTitle.setText("문제집 추가");
 		
 		Container c = getContentPane();
 	}
--- src/Frame.java
+++ src/Frame.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.border.*;
 
-public class Frame extends JFrame implements ActionListener {
+public class Frame extends JFrame implements ActionListener, MouseListener {
 	private static final long serialVersionUID = 1L;
 	@SuppressWarnings("unused")
 	private final ImageIcon imageTest = new ImageIcon("images/GridboxPrototypeTexture.jpg");
@@ -12,35 +12,31 @@ public class Frame extends JFrame implements ActionListener {
 	private final ImageIcon imageReview = new ImageIcon("images/Review.png");
 	private final ImageIcon imageHistory = new ImageIcon("images/History.png");
 	private final ImageIcon imageSetting = new ImageIcon("images/Setting.png");
-	private final ImageIcon imageSearch = new ImageIcon("images/Search.png");
 	private final ImageIcon imageSort = new ImageIcon("images/Sort.png");
 	private final ImageIcon imageAddWorkbook = new ImageIcon("images/AddWorkbook.png");
-	
-	private final String FONT = "맑은 고딕";
-	private final Color COLOR = Color.lightGray;
-	private final Color COLOR_BORDER = Color.gray;
+	private final static String FONT = "맑은 고딕";
+	private final static Color COLOR = Color.lightGray;
+	private final static Color COLOR_BORDER = Color.gray;
 	
 	private Container c;
 	private JPanel pnlNorth = new JPanel();
 	private JPanel pnlNorthEast = new JPanel();
 	private JPanel pnlSouth = new JPanel();
 	private JPanel pnlCenter = new JPanel();
-	private JPanel pnlCenterWest = new JPanel();
 	private JPanel pnlCenterMain = new JPanel();
 	private JLabel lblMenuName = new JLabel(" 문제집");
-	private ImageButton btnSearch = new ImageButton(imageSearch, 50);
 	private ImageButton btnSort = new ImageButton(imageSort, 50);
 	private ImageButton btnAddWorkbook = new ImageButton(imageAddWorkbook, 50);
 	private ImageButton btnWorkbook = new ImageButton(imageWorkbook, 80);
 	private ImageButton btnReview = new ImageButton(imageReview, 80);
 	private ImageButton btnHistory = new ImageButton(imageHistory, 80);
 	private ImageButton btnSetting = new ImageButton(imageSetting, 80);
+	private JTextField tfSearch = new JTextField(15);
 	
-	private WbSearchDlg dlgWbSearch = new WbSearchDlg(this);
 	private WbSortDlg dlgWbSort = new WbSortDlg(this);
 	private WbAddDlg dlgWbAdd = new WbAddDlg(this);
 	
-	private String strMenu = "Workbook";
+	private String strMenu = "";
 	
 	public Frame() {
 		setTitle("나만의 문제집");
@@ -56,21 +52,22 @@ public class Frame extends JFrame implements ActionListener {
 		int y = res.height / 2 - getHeight() / 2;
 		setLocation(x, y);
 		
-		c = getContentPane();
-		c.setLayout( new BorderLayout() );
-		
 		//ImageButton에 ActionListener 추가
 		ImageButton[] aryImageButton = {
-				btnSearch, btnSort, btnAddWorkbook,
-				btnWorkbook, btnReview, btnHistory, btnSetting
+				btnSort, btnAddWorkbook, btnWorkbook, btnReview, btnHistory, btnSetting
 				};
 		for (int i=0; i<aryImageButton.length; i++) {
 			ImageButton imgBtn = aryImageButton[i];
 			imgBtn.setPreferredSize( imgBtn.getSize() );
 			imgBtn.addActionListener(this);
+			imgBtn.addMouseListener(this);
 		}
 		
+		//Gui 그리기
+		c = getContentPane();
+		c.setLayout( new BorderLayout() );
 		c.add(pnlNorth, BorderLayout.NORTH);
+		
 		pnlNorth.setBackground(COLOR);
 		pnlNorth.setBorder( new MatteBorder(0,0,2,0,Color.gray) );
 		pnlNorth.setPreferredSize( new Dimension(0, 70) );
@@ -83,8 +80,6 @@ public class Frame extends JFrame implements ActionListener {
 		pnlNorthEast.setBackground(null);
 		pnlNorthEast.setLayout( new FlowLayout(FlowLayout.RIGHT, 10, 10) );
 		
-		setMenu("Workbook");
-		
 		c.add(pnlSouth, BorderLayout.SOUTH);
 		pnlSouth.setBackground(COLOR);
 		pnlSouth.setBorder( new MatteBorder(2,0,0,0,COLOR_BORDER) );
@@ -92,7 +87,7 @@ public class Frame extends JFrame implements ActionListener {
 		pnlSouth.setLayout( new FlowLayout(FlowLayout.CENTER, 25, 10) );
 		
 		String[] aryButtonName = { "문제집", "오답 노트", "기록", "설정" };
-		for (int i=3; i<aryImageButton.length; i++) {
+		for (int i=2; i<aryImageButton.length; i++) {
 			JPanel pnlTemp = new JPanel();
 			pnlSouth.add(pnlTemp);
 			pnlTemp.setPreferredSize( new Dimension(80, 110) );
@@ -100,7 +95,7 @@ public class Frame extends JFrame implements ActionListener {
 			pnlTemp.setBackground(null);
 			pnlTemp.add(aryImageButton[i], BorderLayout.CENTER);
 			
-			JLabel lblTemp = new JLabel(aryButtonName[i-3], SwingConstants.CENTER);
+			JLabel lblTemp = new JLabel(aryButtonName[i-2], SwingConstants.CENTER);
 			pnlTemp.add(lblTemp, BorderLayout.SOUTH);
 			lblTemp.setPreferredSize( new Dimension(80, 30) );
 			lblTemp.setFont( new Font(FONT, Font.BOLD, 16) );
@@ -108,23 +103,36 @@ public class Frame extends JFrame implements ActionListener {
 		
 		c.add(pnlCenter, BorderLayout.CENTER);
 		pnlCenter.setLayout( new BorderLayout() );
-		pnlCenter.add(pnlCenterWest, BorderLayout.WEST);
-		pnlCenterWest.setPreferredSize( new Dimension(80, 0) );
-		
 		pnlCenter.add(pnlCenterMain, BorderLayout.CENTER);
+		
+		pnlCenterMain.setLayout( new GridBagLayout() );
+		setMenu("Workbook");
+	}
+	
+	public static String getFontName() {
+		return FONT;
+	}
+	
+	public static Color getColor() {
+		return COLOR;
+	}
+	
+	public static Color getColorBorder() {
+		return COLOR_BORDER;
 	}
 	
 	class ImageButton extends JButton {
 		private static final long serialVersionUID = 1L;
 		private ImageIcon image;
-		private int size;
+		private int size = 0;
 		
 		public ImageButton(ImageIcon image, int size) {
 			this.image = image;
 			this.size = size;
+			
 			setSize( new Dimension(size, size) );
 			setBackground(null);
-			setBorderPainted(false);
+			setBorder(null);
 			setContentAreaFilled(false);
 		}
 		
@@ -138,15 +146,31 @@ public class Frame extends JFrame implements ActionListener {
 	public void setMenu(String menu) {
 		ArrayList<ImageButton> listImageButton = new ArrayList<ImageButton>();
 		
+		//이미 해당 메뉴일 경우 종료
+		if (menu == strMenu) {
+			return;
+		}
+		
 		pnlNorthEast.removeAll();
-		pnlCenterMain.removeAll();
+		pnlCenter.removeAll();
 		
 		switch (menu) {
 			case "Workbook":
 				lblMenuName.setText(" 문제집");
-				listImageButton.add(btnSearch);
 				listImageButton.add(btnSort);
 				listImageButton.add(btnAddWorkbook);
+				
+				JPanel pnlWorkbookSearch = new JPanel();
+				pnlWorkbookSearch.setBackground(COLOR);
+				pnlWorkbookSearch.setBorder( new MatteBorder(0,0,2,0,COLOR_BORDER) );
+				pnlWorkbookSearch.setPreferredSize( new Dimension(0, 30) );
+				pnlWorkbookSearch.setLayout( new FlowLayout(FlowLayout.RIGHT, 10, 2) );
+				pnlCenter.add(pnlWorkbookSearch, BorderLayout.NORTH);
+				
+				JLabel lblSearch = new JLabel("검색", SwingConstants.CENTER);
+				lblSearch.setFont( new Font(getFontName(), Font.BOLD, 18) );
+				pnlWorkbookSearch.add(lblSearch);
+				pnlWorkbookSearch.add(tfSearch);
 				break;
 			case "Review":
 				lblMenuName.setText(" 오답 노트");
@@ -172,9 +196,6 @@ public class Frame extends JFrame implements ActionListener {
 		Object source = e.getSource();
 		//상단 메뉴
 		if (strMenu == "Workbook") {
-			if ( source.equals(btnSearch) ) { //검색
-				dlgWbSearch.setVisible(true);
-			}
 			if ( source.equals(btnSort) ) { //정렬
 				dlgWbSort.setVisible(true);
 			}
@@ -196,5 +217,28 @@ public class Frame extends JFrame implements ActionListener {
 		if ( source.equals(btnSetting) ) {
 			setMenu("Setting");
 		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) { }
+
+	@Override
+	public void mousePressed(MouseEvent e) { }
+
+	@Override
+	public void mouseReleased(MouseEvent e) { }
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		c.setCursor( new Cursor(Cursor.HAND_CURSOR) );
+		JButton btn = (JButton)e.getComponent();
+		btn.setBorder( new MatteBorder(1, 1, 1, 1, COLOR_BORDER) );
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		c.setCursor( new Cursor(Cursor.DEFAULT_CURSOR) );
+		JButton btn = (JButton)e.getComponent();
+		btn.setBorder(null);
 	}
 }
