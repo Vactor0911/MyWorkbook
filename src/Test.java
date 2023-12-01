1490186628a5b321641985cd6c6ef5702cb031f1
Vactor0911 <vactor0911@gmail.com>
2023-12-02 05:24:51 +0900

    Updated project files

--- src/Test.java
+++ src/Test.java
@@ -9,15 +9,6 @@ class TestFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setLocation(500, 300);
-		
-		int answer = MessageBox.show(this, "테스트 메시지\n2번째 줄 텍스트\n3번째 줄",
-				MessageBox.btnYES_NO, MessageBox.iconINFORMATION);
-		if (answer == MessageBox.idOK) {
-			System.out.println("확인");
-		}
-		else if(answer == MessageBox.idCANCEL) {
-			System.out.println("취소");
-		}
 	}
 }
 
